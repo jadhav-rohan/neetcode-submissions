e0a885cc50ebc67f70797fba5da18b455a0efd7b
Rohan Jadhav <75313840+jadhav-rohan@users.noreply.github.com>
2026-08-19 17:40:37 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--){
+            if(!st.empty() && temperatures[st.top()] > temperatures[i]){
+                ans[i] = st.top() - i;
+            }
+            else{
+                while(!st.empty() && temperatures[i] >= temperatures[st.top()]){
+                    st.pop();
+                }
+                if(!st.empty() && temperatures[st.top()] > temperatures[i]){
+                    ans[i] = st.top() - i;
+                }else{
+                    ans[i] = 0;
+                }
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
