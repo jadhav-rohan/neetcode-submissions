8d36d2640aeca14285e654c374e02be170aa564b
Rohan Jadhav <75313840+jadhav-rohan@users.noreply.github.com>
2026-08-23 11:24:33 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findHrs(vector<int> &piles, int val){
+        float ans = 0;
+
+        for(int i=0; i<piles.size(); i++){
+            ans += ceil(double(piles[i])/double(val));
+        }
+        return ans;
+    
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(), piles.end());
+        int n = piles.size();
+        int start = 1, end = piles[n-1];    
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            int curr = findHrs(piles, mid);
+            if(curr > h){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+};
