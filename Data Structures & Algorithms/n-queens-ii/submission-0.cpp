91a2af24c8eeef96e0ed4967bb53f05f17330efa
Rohan Jadhav <75313840+jadhav-rohan@users.noreply.github.com>
2026-08-31 18:49:17 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    bool isValid(int row, int col, vector<string> &board, int n){
+        int rowDup = row;
+        int colDup = col;
+
+        while(col >= 0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        col = colDup;
+        row = rowDup;
+
+        while(col >= 0 && row >= 0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+            row--;
+        }
+
+        col = colDup;
+        row = rowDup;
+
+        while(col >= 0 && row < n){
+            if(board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+
+        return true;
+    }
+    void func(vector<string> &board, vector<vector<string>> &ans, int n, int col){
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row=0; row<n; row++){
+            if(isValid(row, col, board, n)){
+                board[row][col] = 'Q';
+                func(board, ans, n, col + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+
+        for(int i=0; i<n; i++){
+            board[i] = s;
+        }
+
+        int col = 0;
+        func(board, ans, n, col);
+
+        return ans.size();
+    }
+};
