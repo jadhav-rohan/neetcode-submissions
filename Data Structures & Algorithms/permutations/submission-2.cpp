7395c321d1b0aca35c3f1629cd511b1e3fdcb713
Rohan Jadhav <75313840+jadhav-rohan@users.noreply.github.com>
2026-09-04 01:00:05 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    void func(int idx, vector<int> nums, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            func(idx + 1, nums, ans);
+            // swap(nums[idx], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+
+        int idx = 0;
+        func(idx, nums, ans);
+        return ans;
+    }
+};
